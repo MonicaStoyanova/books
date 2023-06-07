5ce811e8c5352f27f62ea4320991f7fea69e53a7
MonicaStoyanova <114282401+MonicaStoyanova@users.noreply.github.com>
2023-06-07 12:45:56 +0300

    useCallback

--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useCallback } from "react";
 import axios from "axios";
 
 const BooksContext = createContext();
@@ -7,11 +7,11 @@ const BooksContext = createContext();
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3001/books");
 
     setBooks(response.data);
-  };
+  }, []);
 
   const deleteBookById = async (id) => {
     await axios.delete("http://localhost:3001/books/" + id);
